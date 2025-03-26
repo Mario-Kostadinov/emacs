b8f0c9582c16306c2d2a6dabc62b30c1ab079666
Mario Kostdinov <mmkostadinov@gmail.com>
2025-03-26 07:12:35 +0200

    Fix init custom set variables

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -133,20 +133,3 @@
 ;;(load-file "~/projects/emacs/.emacs.d/satori-packages/prettier.el")
 ;;(load-file "~/projects/emacs/.emacs.d/satori-packages/treesitter.el")
 (load-file "~/projects/emacs/.emacs.d/satori-packages/sassmode.el")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-<<<<<<< Updated upstream
-   '(sudo-edit magit sass-mode lsp-ui web-mode visual-fill-column vertico tree-sitter-langs prettier org-roam org-bullets orderless marginalia lsp-mode exec-path-from-shell doom-themes consult company)))
-=======
-   '(eglot-jl vue-mode all-the-icons-dired vscode-icon dired-sidebar all-the-icons sudo-edit magit sass-mode lsp-ui web-mode visual-fill-column vertico tree-sitter-langs prettier org-roam org-bullets orderless marginalia lsp-mode exec-path-from-shell doom-themes consult company)))
->>>>>>> Stashed changes
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
